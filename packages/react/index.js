7c82d1505d9e395cfd0ebc301f7fea40dcc4bc19
Brandon Webb <Brandon.Webb@vistaprint.com>
2024-08-27 13:53:12 -0400

    fix: disable jsxa11y eslint

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,14 +1,15 @@
 import react from "eslint-plugin-react";
-import jsxA11y from "eslint-plugin-jsx-a11y";
+// FIXME: enable when v9 is supported
+// import jsxA11y from "eslint-plugin-jsx-a11y";
 import { fixupRule } from "@eslint/compat";
 import globals from "globals";
 
 // TODO add eslint-plugin-react-hooks when https://github.com/facebook/react/pull/30774 is merged
 export default [
-    {
-        files: ["**/*.{jsx,mjsx,tsx,mtsx}"],
-        ...fixupRule(jsxA11y.flatConfigs.recommended),
-    },
+    // {
+    //     files: ["**/*.{jsx,mjsx,tsx,mtsx}"],
+    //     ...fixupRule(jsxA11y.flatConfigs.recommended),
+    // },
     {
         files: ["**/*.{jsx,mjsx,tsx,mtsx}"],
         ...react.configs.flat.recommended,
